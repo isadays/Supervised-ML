2299d6ea7c1ef18a2037754477e86df35c026853
Isabela Pereira Lima Dias <59461869+isadays@users.noreply.github.com>
2023-12-12 11:01:07 -0300

    Add files via upload

---
+++ Non-LinearModels/Babies/Script_babiesData.R
@@ -0,0 +1,33 @@
+#Nonlinear Models and Box-Cox Transformations
+
+
+#Isabela Pereira Lima Dias
+
+set_packages <-  c("plotly","tidyverse","ggrepel","fastDummies","knitr","kableExtra",
+                   "splines","reshape2","PerformanceAnalytics","correlation","see",
+                   "ggraph","psych","nortest","rgl","car","ggside","tidyquant","olsrr",
+                   "jtools","ggstance","magick","cowplot","emojifont","beepr","Rcpp",
+                   "equatiomatic")
+
+options(rgl.debug = TRUE)
+#remotes::install_github("abalgo/ggraph", force = TRUE) Force installation
+#install.packages("ggraph", type = "source")
+
+if(sum(as.numeric(!set_packages %in% installed.packages())) != 0){
+  install_packages <- set_packages[!set_packages %in% installed.packages()]
+  for(i in 1:length(install_packages)) {
+    install.packages(install_packages, dependencies = T)
+    break()}
+  sapply(set_packages, require, character = T) 
+} else {
+  sapply(set_packages, require, character = T) 
+}
+
+load(file="babies.RData")
+
+babies <-bebes
+rm(bebes)
+
+
+colnames(babies)[1] <- "length"
+colnames(babies)[2] <- "age" #
